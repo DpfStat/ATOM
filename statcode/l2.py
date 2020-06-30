e385fabb78cff548b78bcb2b8d1d1527865958d7
PF <dpengfei@gmail.com>
2020-06-29 17:09:08 -0700

    lihang code l2

--- statcode/l2.py
+++ statcode/l2.py
@@ -23,3 +23,50 @@ plt.show()
 data = np.array(df.iloc[:100, [0, 1, -1]])
 X, y = data[:,:-1], data[:,-1]
 y = np.array([1 if i == 1 else -1 for i in y])
+
+##### Perceptron #####
+class Model:
+    def _int_(self):
+        self.w = np.ones(len(data[0]) - 1, dtype=np.float32)
+        self.b = 0
+        self.l_rate = 0.1
+        # self.data = data
+
+    # y
+    def sign(self, x, w, b):
+        y = np.dot(x, w) + b
+        return y
+
+   # gradient descent
+    def fit(self, X_train, y_train):
+        is_wrong = False
+        while not is_wrong:
+            wrong_count = 0
+            for d in range(len(X_train)):
+                X = X_train[d]
+                y = y_train[d]
+                if y * self.sign(X, self.w, self.b) <= 0:
+                    self.w = self.w + self.l_rate * np.dot(y, X)
+                    self.b = self.b + self.l_rate * y
+                    wrong_count += 1
+            if wrong_count == 0:
+                is_wrong = True
+        return 'Perceptron Model!'
+
+    def score(self):
+        pass
+
+perceptron = Model()
+perceptron.fit(X, y)
+
+# find the line
+x_points = np.linspace(4, 7, 10)
+y_ = -(perceptron.w[0] * x_points + perceptron.b) / perceptron.w[1]
+
+plt.plot(x_points, y_)
+plt.plot(data[:50, 0], data[:50, 1], 'bo', color='blue', label='0')
+plt.plot(data[50:100, 0], data[50:100, 1], 'bo', color='orange', label='1')
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+plt.show()
---
+++ statcode/l2_example.py
@@ -0,0 +1,47 @@
+import sklearn
+from sklearn.linear_model import Perceptron
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns = iris.feature_names)
+df['label']=iris.target
+df.columns = [
+    'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+]
+df.label.value_counts()
+
+data = np.array(df.iloc[:100, [0, 1, -1]])
+X, y = data[:,:-1], data[:,-1]
+y = np.array([1 if i == 1 else -1 for i in y])
+
+clf = Perceptron(fit_intercept = True,
+                 max_iter = 1000,
+                 shuffle = True)
+clf.fit(X,y)
+
+# get the weights assigned to the features
+print(clf.coef_)
+# intercept
+print(clf.intercept_)
+
+plt.figure(figsize =(10, 10))
+plt.scatter(data[:50, 0], data[:50, 1], c='b', label='Iris-setosa',)
+plt.scatter(data[50:100, 0], data[50:100, 1], c='orange', label='Iris-versicolor')
+x_ponits = np.arange(4, 8)
+y_ = -(clf.coef_[0][0]*x_ponits + clf.intercept_)/clf.coef_[0][1]
+plt.plot(x_ponits, y_)
+
+plt.legend()
+plt.grid(False)
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+plt.show()
+
+#clf = Perceptron(fit_intercept = True,
+#                 max_iter = 1000,
+#                 tol = None,
+#                 shuffle = True)
+# tol is the argument to decide when to stop, tol = 0.001 for the default
